81949dcd13139c72a442a34c9e4be0769b004b5c
aronresni <aronresni@gmail.com>
2023-07-29 18:11:14 -0300

    Upload Projesct

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import NavBar from './components/NavBar';
 import SobreMi from './components/SobreMi';
 import Stack from './components/Stack';
 import Footer from "./components/Footer"
+import Proyectos from './components/Proyectos/Proyectos';
 function App() {
   return (
     <div className="App select-none">
       <NavBar />
       <SobreMi />
       <Stack />
-      <Footer/>
+      <Proyectos />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Proyectos/modalProyectos.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const modalProyectos = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default modalProyectos
